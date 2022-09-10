c7634359807f729b17829a277e018a7daa9b3bb4
Kamilla Antunes <kamilla.antunes@mercadolivre.com>
2022-09-09 22:16:49 -0300

    feature: creating and configuring MainTabBar

---
+++ Netflix Clone/Netflix Clone/Controllers/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  Netflix Clone
+//
+//  Created by Kamilla Mylena Teixeira Antunes on 09/09/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+}
---
+++ Netflix Clone/Netflix Clone/Controllers/UpComingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  UpComingViewController.swift
+//  Netflix Clone
+//
+//  Created by Kamilla Mylena Teixeira Antunes on 09/09/22.
+//
+
+import UIKit
+
+class UpComingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+}
